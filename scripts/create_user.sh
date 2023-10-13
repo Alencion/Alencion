5f4ff6da6ce9d8d5d0a889cbf98053635ec159b5
이정준/협업서비스개발그린팀/ND <jeongjun.lee@nhndooray.com>
2023-10-13 14:20:13 +0900

    modify script

--- scripts/create_user.sh
+++ scripts/create_user.sh
@@ -4,6 +4,6 @@ USER_NAME=$1
 
 echo $USER_NAME
 mysql -h127.0.0.1 --port 3306 -uroot -p123456 -e "create user '$USER_NAME'@'%' identified with mysql_native_password" --ssl-mode=disabled
-mysql -h127.0.0.1 --port 33060 -uroot -p123456 -e "alter user '$USER_NAME'@'%' identified by '$USER_NAME'" --ssl-mode=disabled
-mysql -h127.0.0.1 --port 33060 -uroot -p123456 -e "grant ALL PRIVILEGES on *.* to '$USER_NAME'@'%'" --ssl-mode=disabled
-mysql -h127.0.0.1 --port 33060 -uroot -p123456 -e "flush privileges" --ssl-mode=disabled
+mysql -h127.0.0.1 --port 3306 -uroot -p123456 -e "alter user '$USER_NAME'@'%' identified by '$USER_NAME'" --ssl-mode=disabled
+mysql -h127.0.0.1 --port 3306 -uroot -p123456 -e "grant ALL PRIVILEGES on *.* to '$USER_NAME'@'%'" --ssl-mode=disabled
+mysql -h127.0.0.1 --port 3306 -uroot -p123456 -e "flush privileges" --ssl-mode=disabled
